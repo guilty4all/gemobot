00783d2d67193dc83496b15c82e7a98a5cafcc6d
guilty4all <guilty4all@gmail.com>
2022-02-19 20:40:10 +0600

    update

--- main.go
+++ main.go
@@ -1,1 +1,78 @@
-package gemobot
+package main
+
+import (
+	"encoding/json"
+	tba "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+	_ "github.com/lib/pq"
+)
+
+var (
+	bot      *tba.BotAPI
+	botToken = os.Getenv(Bot_Token)
+	baseURL  = os.Getenv(App_URL)
+)
+
+func initTelegram() {
+	var err error
+
+	bot, err = tba.NewBotAPI(botToken)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// this perhaps should be conditional on GetWebhookInfo()
+	// only set webhook if it is not set properly
+	url := baseURL + bot.Token
+	_, err = bot.SetWebhook(tba.NewWebhook(url))
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func webhookHandler(c *gin.Context) {
+	defer c.Request.Body.Close()
+
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	var update tba.Update
+	err = json.Unmarshal(bytes, &update)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// to monitor changes run: heroku logs --tail
+	log.Printf("From: %+v Text: %+v\n", update.Message.From, update.Message.Text)
+}
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	// gin router
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	// telegram
+	initTelegram()
+	router.POST("/"+bot.Token, webhookHandler)
+
+	err := router.Run(":" + port)
+	if err != nil {
+		log.Println(err)
+	}
+}
